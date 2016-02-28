903d19f233c3fce51ba61bc9c29e307d5426612f
Jefferson Mendoza <jefferson.mendoza@correounivalle.edu.co>
2016-02-28 16:36:14 -0500

    commit

--- exe.sh
+++ exe.sh
@@ -3,14 +3,10 @@
 
 echo "Esta ejecutando el algoritmo las vegas para resolver el sudoku... espere un momento\n\n"
 
-for a in `seq 1 5`; do
-	echo "Sudoku No. ==>"  $a"%"
-    /opt/jdk1.8.0_60/bin/java -jar dist/Sudoku.jar sudoku.txt & sleep 300 && kill $! 2>/dev/null && echo "myProgram didn't finish"
-done
+
+/opt/jdk1.8.0_60/bin/java -jar dist/Sudoku.jar sudoku10.txt 10 
+
 
 
 echo "*************************************************\n\n"
- echo "Se ejecuto exitosamente, revise los archivos que corresponden a cada escenario:\n
-       \t banco-1.csv \n
-       \t banco-6.csv \n 
-       \t banco-7.csv \n\n"
+ echo "Se ejecuto exitosamente:\n"
